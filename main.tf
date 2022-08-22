43440a582303309062e0eea19d185f0093feb749
Omer Abbadi <100992264+abbadis@users.noreply.github.com>
2022-08-22 08:36:27 +0530

    Update
    Update

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
     region = var.AWS_REGION
+    access_key="AKIA4T5FA7WRRDYBOF6M"
+    secret_key="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"
 }
 resource "aws_instance" "module-demo" {
     ami = var.AWS_AMI
